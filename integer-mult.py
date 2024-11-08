c7ab1256f4bc96720aaac7c96916807170167ec5
vermilion10 <fyassarahman@gmail.com>
2024-11-08 10:28:36 +0700

    basic i/o with python

---
+++ integer-mult.py
@@ -0,0 +1,9 @@
+'''
+desc: integer multiplication
+author: vermilion10
+date: november 2024
+'''
+x = int(input('input x: '))
+y = int(input('input y: '))
+nilai = x * y
+print('hasil dari', x, 'dikali', y , 'adalah', nilai)
