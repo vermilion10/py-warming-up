77c85af56c5cfe175b25b5a01baafbbdef26e44d
vermilion10 <fyassarahman@gmail.com>
2024-11-10 17:28:08 +0700

    basic I/O 3 with py lang

---
+++ tempconvert.py
@@ -0,0 +1,8 @@
+'''
+desc: temprature conversion from celcius to fahrenheit
+author: vermilion10
+date: november 2024
+'''
+celcius = float(input('input celcius: '))
+fahrenheit = (celcius * 9/5) + 32
+print(f'{celcius} C equal to {fahrenheit}')
