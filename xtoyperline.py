1c547cc52ccfd12981b8fc7c705c224e7f85f4df
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-17 12:49:35 +0700

    xty

---
+++ xtoyperline.py
@@ -0,0 +1,12 @@
+'''
+desc: show a series of numbers in a row
+author: vermilion10
+date: november 2024
+'''
+
+x = int(input("input x: "))
+y = int(input("input y: "))
+
+for i in range(x, y + 1):
+    print(i)
+
